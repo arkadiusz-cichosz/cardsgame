a93af8ade8e641909a861ad5910bbb8fba96699c
Arkadiusz Cichosz <A.Cichosz@pwpw.pl>
2018-01-08 10:00:24 +0100

    class change

--- src/main/java/org/pwpw/game/controller/MainController.java
+++ src/main/java/org/pwpw/game/controller/MainController.java
@@ -28,28 +28,6 @@ public class MainController {
  @Autowired
  private PlayerRepository playerRepository;
  
- int i = 0;
- 
- @Autowired
- Game game;
-
- @GetMapping("/status")
- public String gameStatus() {
-  ObjectMapper mapper = new ObjectMapper();
-  List<Player> players = new ArrayList<>();
-  players.add(new Player("Arek", "000AF11BA"));
-  game.setPlayers(players);
-  try {
-   String jsonString = mapper.writeValueAsString(playerRepository);
-   System.out.println("jsonString = " + jsonString + " " + i++);
-   return jsonString;
-  } catch (JsonProcessingException e) {
-   e.printStackTrace();
-   return null;
-  }
- }
- 
- 
  @PostMapping("/addUser")
  public String addUser(@RequestParam(value = "name", required = false, defaultValue = "user#") String name, Model model, HttpSession session) {
   playerRepository.createPlayer(session.getId(), name);
